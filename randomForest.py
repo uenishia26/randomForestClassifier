b1addfd5935f6afe66513912d5bcae47ff90f85f
Anamu Uenishi <123331064+uenishia26@users.noreply.github.com>
2024-06-21 16:11:40 -0400

    Add files via upload

--- randomForest.py
+++ randomForest.py
@@ -8,32 +8,43 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import f1_score
 from sklearn.metrics import classification_report
-from sklearn.tree import plot_tree
-
-iris = load_iris()
-
-df = pd.DataFrame(iris.data, columns=iris.feature_names)
-df ['species'] = pd.Categorical.from_codes(iris.target, iris.target_names) #Just adding label name 
-
-X, y = df.iloc[:,:4], df.iloc[:,4]
-#print(df['species'].unique())
-
-#Before labelEncoding indexing order is [setosa, versicolor, virginica]
-
-lEncoder = LabelEncoder()
-y = lEncoder.fit_transform(y)
-
-xtrain, xtest, ytrain, ytest = train_test_split(X,y, test_size=0.2, random_state=42)
-
-clf = RandomForestClassifier(n_jobs=2, random_state=10, n_estimators=10)
-clf.fit(xtrain, ytrain)
-ypredict = clf.predict(xtest)
+from sklearn.tree import plot_tree #Plot tree sklearn module allows us to plot trees
+from imblearn.ensemble import BalancedBaggingClassifier
+from sklearn.model_selection import StratifiedKFold
 
 
-singularTree = clf.estimators_[0]
-plt.figure(figsize=(12,8))
-plot_tree(singularTree, feature_names=iris.feature_names, class_names=iris.target_names, filled=True)
-plt.show()
+df = pd.read_csv("formatedData.csv")
 
+X,y = df.iloc[:,:8], df.iloc[:,-1]
+
+"""
+    By looking at the classification report and in particular the f1 score, 
+    and take the avg div by numSplits you can see which model performed better 
+    There are two classifiers use both to train and predict 
+    
+"""
+
+skf = StratifiedKFold(shuffle=True, random_state=42, n_splits=5)
+rfc = RandomForestClassifier(random_state=42, n_estimators=100)
+bbClassifier = BalancedBaggingClassifier(rfc,sampling_strategy='auto',replacement=False,random_state=42)
+sumMacroF1Score = 0
+
+for train_index, test_index in skf.split(X,y): 
+    xtrain = X.iloc[train_index,:8]
+    xtest = X.iloc[test_index, :8]
+    ytrain = y.iloc[train_index]
+    ytest = y.iloc[test_index]
+    bbClassifier.fit(xtrain, ytrain)
+    ypredict = bbClassifier.predict(xtest)
+    print(classification_report(ytest,ypredict))
+
+
+""""
+    *** Plotting Trees ***
+        singularTree = clf.estimators_[0]
+        plt.figure(figsize=(12,8))
+        plot_tree(singularTree, feature_names=iris.feature_names, class_names=iris.target_names, filled=True)
+        plt.show()
+"""
 
 
